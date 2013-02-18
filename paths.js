619fb0f56debb890d812657fe72377511fe6f5bb
etai <etai@empeeric.com>
2013-02-18 15:40:18 +0200

    bind admin instance to it's methods so this will be defined

--- paths.js
+++ paths.js
@@ -6,15 +6,15 @@ var url_join = exports.testable_url_join = function () { return path.join.apply(
 
 exports.registerPaths = function (admin, app, root) {
     root = root || '/';
-    app.get(root, admin.index);
-    app.get(url_join(root, '/login'), admin.login);
-    app.get(url_join(root, '/logout'), admin.logout);
-    app.get(url_join(root, '/model/:modelName'), admin.model);
-    app.get(url_join(root, '/model/:modelName/document/:documentId'), admin.document);
-    app.post(url_join(root, '/model/:modelName/document/:documentId'), admin.documentPost);
-    app.post(url_join(root, '/json/login'), admin.loginPost);
-    app.post(url_join(root, '/json/dependencies'), admin.checkDependencies);
-    app.post(url_join(root, '/json/model/:collectionName/order'), admin.orderDocuments);
-    app.post(url_join(root, '/json/model/:modelName/action/:actionId'), admin.actionDocuments);
-    app.delete(url_join(root, '/json/model/:collectionName/document'), admin.deleteDocument);
+    app.get(root, admin.index.bind(admin));
+    app.get(url_join(root, '/login'), admin.login.bind(admin));
+    app.get(url_join(root, '/logout'), admin.logout.bind(admin));
+    app.get(url_join(root, '/model/:modelName'), admin.model.bind(admin));
+    app.get(url_join(root, '/model/:modelName/document/:documentId'), admin.document.bind(admin));
+    app.post(url_join(root, '/model/:modelName/document/:documentId'), admin.documentPost.bind(admin));
+    app.post(url_join(root, '/json/login'), admin.loginPost.bind(admin));
+    app.post(url_join(root, '/json/dependencies'), admin.checkDependencies.bind(admin));
+    app.post(url_join(root, '/json/model/:collectionName/order'), admin.orderDocuments.bind(admin));
+    app.post(url_join(root, '/json/model/:modelName/action/:actionId'), admin.actionDocuments.bind(admin));
+    app.delete(url_join(root, '/json/model/:collectionName/document'), admin.deleteDocument.bind(admin));
 };
